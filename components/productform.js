cc6e99ffef18d4eebf075bb12a08f514eb3e9fbe
Vivy <Tomitaviorel@yahoo.com>
2023-04-19 14:38:02 +0300

    feat: upload picture block

--- components/productform.js
+++ components/productform.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-const ProductForm = ({ _id, title: existingTitle, description: existingDescription, price: existingPrice }) => {
+const ProductForm = ({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images }) => {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
@@ -24,10 +24,41 @@ const ProductForm = ({ _id, title: existingTitle, description: existingDescripti
     router.push('/products')
   }
 
+  const uploadImage = async (e) => {
+    const files = e.target?.files;
+    console.log(files, 'files')
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file)
+      }
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      })
+      console.log(res)
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
       <input type='text' placeholder='product name' value={title} onChange={e => setTitle(e.target.value)} />
+      <label>Photos
+      </label>
+      <div className='mb-2'>
+        <label className='w-24 h-24 flex justify-center items-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+          </svg>
+          <div>
+            Upload
+          </div>
+          <input type='file' className='hidden' onChange={uploadImage} />
+        </label>
+        {!images?.length && (
+          <div>No photos for this product</div>
+        )}
+      </div>
       <label>Description</label>
       <textarea placeholder='description' value={description} onChange={e => setDescription(e.target.value)}></textarea>
       <label>Price (in USD)</label>
---
+++ pages/api/upload.js
@@ -0,0 +1,21 @@
+import multiparty from 'multiparty'
+
+const handle = async (req, res) => {
+  const form = new multiparty.Form()
+  const { fields, files } = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err)
+      resolve({ fields, files })
+    })
+  })
+  console.log(files.file.length)
+
+  res.json('ok')
+}
+
+export default handle
+
+
+export const config = {
+  api: { bodyParser: false }
+}
