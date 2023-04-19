e931c1dbeca6c5dccabb2b8092a62ac6cd3ff468
Vivy <Tomitaviorel@yahoo.com>
2023-04-19 12:53:39 +0300

    feat: update functionallity added

--- components/productform.js
+++ components/productform.js
@@ -2,23 +2,30 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-const ProductForm = ({ title: existingTitle, description: existingDescription, price: existingPrice }) => {
+const ProductForm = ({ _id, title: existingTitle, description: existingDescription, price: existingPrice }) => {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
   const [goToProducts, setGoToProducts] = useState(false)
   const router = useRouter()
-  const handleSubmit = async (e) => {
+
+  const saveProduct = async (e) => {
     e.preventDefault()
     const data = { title, description, price };
-    await axios.post('/api/products', data)
+    if (_id) {
+      await axios.put('/api/products', { ...data, _id })
+    } else {
+      await axios.post('/api/products', data)
+    }
     setGoToProducts(true)
   }
+
   if (goToProducts) {
     router.push('/products')
   }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={saveProduct}>
       <label>Product name</label>
       <input type='text' placeholder='product name' value={title} onChange={e => setTitle(e.target.value)} />
       <label>Description</label>
--- pages/api/products.js
+++ pages/api/products.js
@@ -12,12 +12,19 @@ const handle = async (req, res) => {
       res.json(await Product.find())
     }
   }
+
   if (method === 'POST') {
     const { title, description, price } = req.body;
     const productDoc = await Product.create({
       title, description, price
     })
     res.json(productDoc)
+  }
+
+  if (method === 'PUT') {
+    const { title, description, price, _id } = req.body;
+    await Product.updateOne({ _id }, { title, description, price })
+    res.json(true)
   }
 }
 
